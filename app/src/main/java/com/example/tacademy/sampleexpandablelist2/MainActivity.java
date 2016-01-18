c17c3dfff5c32368cda78811ab56e44bb784bb99
kiss9815 <kiss9815@gmail.com>
2016-01-18 19:17:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/sampleexpandablelist2/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tacademy.sampleexpandablelist2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
